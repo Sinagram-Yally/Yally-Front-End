2a71d780e469541da152b40bdb0a000521d9f588
Srim <deamoon0801@naver.com>
2020-09-14 16:14:53 +0900

    Main 기본 컴포넌트 생성

---
+++ yally/src/components/Main/AddTimeLine.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AddTimeLine = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default AddTimeLine;
---
+++ yally/src/components/Main/TimeLineView.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {background ,moreButton } from '../../assets/img'
+import '../../assets/style/background.css'
+
+const TimeLineView = () => {
+    return (
+        <div>
+            <img src={background} className="backgroundImg"></img>
+        </div>
+    );
+};
+
+export default TimeLineView;
