730a108a96557e16c7a0342b426f9519a4d29827
Srim <deamoon0801@naver.com>
2020-09-15 22:03:07 +0900

    [ADD] PostItem UI

---
+++ yally/src/assets/style/PostItmes.js
@@ -0,0 +1,74 @@
+import styled from 'styled-components'
+
+export const postInfoContainer = styled.div`
+    display: flex;
+`;
+
+export const postNameInfo = styled.h1`
+    font-size: 25px;
+    color: #707070;
+    font-weight: 400;
+    margin : 3px 0px 0px 0px;
+`;
+
+export const postDateInfo = styled.p`
+    font-size: 20px;
+    color: #707070;
+    font-weight: 200;
+    margin : 3px 0px 0px 0px;
+`;
+
+export const postInfoBox = styled.div`
+    margin : 10px 0px 50px 20px;
+`;
+
+export const postSection = styled.section`
+    width: 100%;
+    height: 400px;
+    background-color: #000000;
+    opacity: 0.5;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
+export const postArticle = styled.article`
+    width: 440px;
+`;
+
+export const playIcon = styled.img`
+
+`;
+
+export const playInfoBox = styled.div`
+    display: flex;
+    justify-content: center;
+`;
+
+export const postWritten = styled.p`
+    font-weight: 400;
+    font-size: 23px;
+    color : #FFFFFF;
+    margin-bottom: 20px;
+`;
+
+export const reactionContainer = styled.div`
+    margin-top : 20px;
+    display: flex;
+    align-items: center;
+`;
+
+export const reactionBox = styled.div`
+    display: flex;
+    align-items: center;
+    margin-right: 35px;
+`;
+
+export const reactionIcon = styled.img`
+    margin-right : 5px;
+`;
+
+export const reactionCount = styled.p`
+    color : #707070;
+    margin : 0;
+`;
--- yally/src/components/Main/PostItem.js
+++ yally/src/components/Main/PostItem.js
@@ -1,10 +1,42 @@
 import React from 'react';
+import * as S from "../../assets/style/AddTimeLine";
+import * as P from "../../assets/style/PostItmes"
+import { playButton, yallyOff, repl } from '../../assets/img'
 
 const PostItem = () => {
     return (
-        <div>
-            
-        </div>
+        <S.mainContainer>
+            <S.mainSection>
+                <P.postInfoContainer>
+                    <S.profileImg>
+                    </S.profileImg>
+                    <P.postInfoBox>
+                        <P.postNameInfo>대충어떤닉네임</P.postNameInfo>
+                        <P.postDateInfo>2020년 1월 1일</P.postDateInfo>
+                    </P.postInfoBox>
+                    
+               </P.postInfoContainer>
+                <P.postSection>
+                    <P.postArticle>
+                        <P.postWritten>글 작성 어쩌구저쩌구~~ #첫게시물 #LIKE #얄리</P.postWritten>
+                        <P.playInfoBox>
+                            <P.playIcon src={playButton}></P.playIcon>
+                        </P.playInfoBox>
+                        
+                    </P.postArticle>
+                </P.postSection>
+                <P.reactionContainer>
+                    <P.reactionBox>
+                        <P.reactionIcon src={yallyOff}></P.reactionIcon>
+                        <P.reactionCount>19개</P.reactionCount>
+                    </P.reactionBox>
+                    <P.reactionBox>
+                        <P.reactionIcon src={repl}></P.reactionIcon>
+                        <P.reactionCount>21개</P.reactionCount>
+                    </P.reactionBox>
+                </P.reactionContainer>    
+            </S.mainSection>
+        </S.mainContainer>
     );
 };
 
